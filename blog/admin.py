247fe6c1d67d15987256ad5f74e58a7c87edfd27
Shakhboz07 <shahbozrakhmatov07@gmail.com>
2022-11-28 13:57:47 +0500

    ready

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from blog.models import Post
+
+# Register your models here.
+
+admin.site.register(Post)
---
+++ blog/urls.py
@@ -0,0 +1,27 @@
+"""root URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+from django.contrib import admin
+from django.urls import path
+
+from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogEditView, BlogDeleteView
+
+urlpatterns = [
+    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', BlogEditView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+]
